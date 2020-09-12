ad40a2f2547784f261d3db40218d54d702c0a9c6
milo1150 <oosamuoo02@gmail.com>
2020-09-13 00:24:02 +0700

    first commit toei

---
+++ app/api/dummy/brand.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: '2FAST',
+  desc: 'Enlite Prime - React.js Fullstack Template',
+  prefix: 'enlite',
+  footerText: 'Enlite Prime All Rights Reserved 2018',
+  logoText: 'Enlite Prime',
+};
